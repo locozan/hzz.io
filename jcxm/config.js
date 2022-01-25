e9cee257ac4cc6fbce9982b8b8a46433d9f01eec
wb-hzz271212 <wb-hzz271212@alibaba-inc.com>
2022-01-25 15:29:05 +0800

    代码保存

---
+++ jcxm/config.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: your name
+ * @Date: 2019-11-26 15:23:30
+ * @LastEditTime: 2020-03-13 18:25:52
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /mobile-webpack/public/config.js
+ */
+
+var a = function(){
+    // 31测试环境
+    this.BaseUrl = ''
+    // 添加二级域名为空
+    this.BasePath = '';
+    // ‘我的’模块，用户头像地址
+    this.imgUrl = 'https://oa.m.tencent.com/an:insurance/ts:r.hrc.oa.com/photo/100/'
+};
---
+++ writing/eg-ipad/model.js
@@ -0,0 +1,227 @@
+var desktopsModel = [
+[
+      { 'title': '宝贝',
+        'img': '/writing/eg-ipad/images/babi.png',
+        'handler': function(){}
+      },
+      { 'title': '拯救萝卜', 
+        'img': '/writing/eg-ipad/images/baluobo.png',
+        'handler': function(){}
+      },
+      // { 'title': '棒球男孩',
+      //   'img': '/writing/eg-ipad/images/bangqiunanhai.png', 
+      //   'handler': function(){}
+      // },
+      // { 'title': '冰雪奇缘',
+      //   'img': '/writing/eg-ipad/images/bingxueqiyuan.png',
+      //   'handler': function(){}
+      // },
+      // { 'title': 'buluo',
+      //   'img': '/writing/eg-ipad/images/buluo.png',
+      //   'handler': function(){}
+      // },
+      { 'title': 'fishing',
+        'img': '/writing/eg-ipad/images/buyu.png',
+        'handler': function(){}
+      },
+      { 'title': 'songsu',
+        'img': '/writing/eg-ipad/images/songsu.png',
+        'handler': function(){}
+      },
+      { 'title': 'chaoren',
+        'img': '/writing/eg-ipad/images/chaoren.png',
+        'handler': function(){}
+      },
+      { 'title': 'chuiqiu',
+        'img': '/writing/eg-ipad/images/chuiqiu.png',
+        'handler': function(){}
+      },
+      { 'title': 'cike',
+        'img': '/writing/eg-ipad/images/cike.png',
+        'handler': function(){}
+      },
+      { 'title': '弹珠',
+        'img': '/writing/eg-ipad/images/danzhu.png',
+        'handler': function(){}
+      },
+      { 'title': 'block breaker',
+        'img': '/writing/eg-ipad/images/dazhuankuai.png',
+        'handler': function(){}
+      },
+      { 'title': 'dongwuzhanzheng',
+        'img': '/writing/eg-ipad/images/dongwuzhanzheng.png',
+        'handler': function(){}
+      },
+      { 'title': '豆掰',
+        'img': '/writing/eg-ipad/images/douban.png',
+        'handler': function(){}
+      },
+      { 'title': 'gaoxiaonanhai',
+        'img': '/writing/eg-ipad/images/gaoxiaonanhai.png',
+        'handler': function(){}
+      },
+      { 'title': '优顾炒股',
+        'img': '/writing/eg-ipad/images/gupiao.png',
+        'handler': function(){}
+      },
+      { 'title': 'Blackbird',
+        'img': '/writing/eg-ipad/images/heiji.png',
+        'handler': function(){}
+      }
+    ],
+    [
+    { 'title': 'huaxue',
+        'img': '/writing/eg-ipad/images/huaxue.png',
+        'handler': function(){}
+      },
+      { 'title': 'huoying',
+        'img': '/writing/eg-ipad/images/huoying.png',
+        'handler': function(){}
+      },
+      { 'title': '爱奇艺',
+        'img': '/writing/eg-ipad/images/iqiyi.png',
+        'handler': function(){}
+      },
+      { 'title': 'sumabaobei',
+        'img': '/writing/eg-ipad/images/sumabaobei.png',
+        'handler': function(){}
+      },
+      { 'title': 'tankedazhan',
+        'img': '/writing/eg-ipad/images/tankedazhan.png',
+        'handler': function(){}
+      },
+      { 'title': '淘宝',
+        'img': '/writing/eg-ipad/images/taobao.png',
+        'handler': function(){}
+      },
+      { 'title': 'UC',
+        'img': '/writing/eg-ipad/images/uc.png',
+        'handler': function(){}
+      },
+      { 'title': 'waitaikong',
+        'img': '/writing/eg-ipad/images/waitaikong.png',
+        'handler': function(){}
+      },
+      { 'title': 'wangyinew',
+        'img': '/writing/eg-ipad/images/wangyinew.png',
+        'handler': function(){}
+      }
+      ],
+      [
+      { 'title': 'jisusaiche',
+        'img': '/writing/eg-ipad/images/jisusaiche.png',
+        'handler': function(){}
+      },
+      { 'title': '机翼',
+        'img': '/writing/eg-ipad/images/jiyinv.png',
+        'handler': function(){}
+      },
+      { 'title': 'wangyiyun',
+        'img': '/writing/eg-ipad/images/wangyiyun.png',
+        'handler': function(){}
+      },
+      { 'title': '中国象棋',
+        'img': '/writing/eg-ipad/images/xiangqi.png',
+        'handler': function(){}
+      },
+      { 'title': '小女孩',
+        'img': '/writing/eg-ipad/images/xiaonvhai.png',
+        'handler': function(){}
+      }
+      ],
+      [
+      { 'title': 'meinvchuanqi',
+        'img': '/writing/eg-ipad/images/meinvchuanqi.png',
+        'handler': function(){} 
+      },
+      { 'title': 'meiyanxiangji',
+        'img': '/writing/eg-ipad/images/meiyanxiangji.png', 
+        'handler': function(){}
+      },
+      { 'title': 'milaosu',
+        'img': '/writing/eg-ipad/images/milaosu.png',
+        'handler': function(){}
+      },
+      { 'title': 'Car Wash',
+        'img': '/writing/eg-ipad/images/xiche.png',
+        'handler': function(){} 
+      },
+      { 'title': 'xingpan',
+        'img': '/writing/eg-ipad/images/xingpan.png', 
+        'handler': function(){}
+      },
+      { 'title': 'Panda',
+        'img': '/writing/eg-ipad/images/xiongmao.png',
+        'handler': function(){}
+      },
+      { 'title': 'xuerendazhan',
+        'img': '/writing/eg-ipad/images/xuerendazhan.png',
+        'handler': function(){}
+      },
+      { 'title': 'yingxong',
+        'img': '/writing/eg-ipad/images/yingxong.png',
+        'handler': function(){} 
+      },
+      { 'title': '有道词典',
+        'img': '/writing/eg-ipad/images/youdao.png', 
+        'handler': function(){}
+      },
+      { 'title': 'yuehui',
+        'img': '/writing/eg-ipad/images/yuehui.png',
+        'handler': function(){}
+      }
+      ],
+      [
+      { 'title': 'nvhaihexiong',
+        'img': '/writing/eg-ipad/images/nvhaihexiong.png',
+        'handler': function(){} 
+      },
+      { 'title': 'oneps',
+        'img': '/writing/eg-ipad/images/oneps.png',
+        'handler': function(){}
+      },
+      { 'title': 'QQ',
+        'img': '/writing/eg-ipad/images/qq.png',
+        'handler': function(){}
+      },
+      { 'title': 'quanji',
+        'img': '/writing/eg-ipad/images/quanji.png',
+        'handler': function(){} 
+      },
+      { 'title': '炉石传说',
+        'img': '/writing/eg-ipad/images/rushi.png',
+        'handler': function(){}
+      },
+      { 'title': 'shibing',
+        'img': '/writing/eg-ipad/images/shibing.png',
+        'handler': function(){}
+      },
+      { 'title': 'sina',
+        'img': '/writing/eg-ipad/images/sina.png',
+        'handler': function(){} 
+      },
+      { 'title': 'sishen',
+        'img': '/writing/eg-ipad/images/sishen.png',
+        'handler': function(){}
+      }
+      ]
+]
+
+    var dockModel = [
+      { 'title': '电话',
+        'img': '/writing/eg-ipad/images/call.png',
+        'handler': function(){}
+      },
+      { 'title': '微信',
+        'img': '/writing/eg-ipad/images/weixin.png',
+        'handler': function(){}
+      },
+      // { 'title': '支付宝',
+      //   'img': '/writing/eg-ipad/images/zhifubao.png',
+      //   'handler': function(){}
+      // },
+      { 'title': 'smule',
+        'img': '/writing/eg-ipad/images/smule.png',
+        'handler': function(){}
+      }
+    ]
